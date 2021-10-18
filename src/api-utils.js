1ec3e03b82b99430d52af78e1b2b97ab9f2763d6
diyanamendoza <diyana.amp@pm.me>
2021-10-18 10:21:21 -0700

    Add api utils, auth stuff

---
+++ src/api-utils.js
@@ -0,0 +1,81 @@
+import request from 'superagent';
+
+const URL = 'https://shielded-savannah-39977.herokuapp.com/';
+
+//sign up
+export async function signUp(email, password) {
+  const response = await request 
+    .post(`${URL}auth/signup`)
+    .send({ 'email': email, 'password': password });
+  return response.body;
+}
+
+//login
+export async function logIn(email, password) {
+  const response = await request 
+    .post(`${URL}auth/signin`)
+    .send({ 'email': email, 'password': password });
+  return response.body;
+}
+
+//get pets
+export async function getPets(token) {
+  const response = await request
+    .get(`${URL}api/pets`)
+    .set('Authorization', token);
+  return response.body;
+}
+
+//post pet
+export async function createPet(pet, token) {
+  const response = await request
+    .post(`${URL}api/pets`)
+    .set('Authorization', token)
+    .send(pet);
+  return response.body;
+}
+
+//delete pet
+export async function deletePet(id, token) {
+  const response = await request
+    .delete(`${URL}/api/pets/${id}`)
+    .set('Authorization', token);
+  return response.body;
+}
+
+
+//update pet...update is an object {name: blah, birthday:blah}
+export async function updatePet(id, update, token) {
+  const response = await request
+    .put(`${URL}api/pets/${id}`)
+    .set('Authorization', token)
+    .send({
+      'id': id,
+      'name': update.name,
+      'birthday': update.birthday
+    });
+  return response.body;
+}
+
+//get sign
+export async function getSign(birthday){
+  const response = await request 
+    .get(`${URL}/sign?date=${birthday}`);
+  return response.body;
+}
+
+//get horoscope
+export async function getHoroscope(sign){
+  const response = await request 
+    .post(`${URL}/horoscope?sign=${sign}`);
+  return response.body;
+}
+
+//yet yelp data
+export async function getYelpData(location){
+  const response = await request 
+    .get(`${URL}/yelp?location=${location}`);
+  return response.body;
+}
+
+//add stuff for getting and updating PS user profile info (name, location, birthday, sign)
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import { signUp, logIn } from '../api-utils.js';
 
 export default class HomePage extends Component {
+  state = {
+    password: '',
+    email: '',
+    error: ''
+  }
+
+  //signup submit - need to add actions for adding PS user data (user profile) <--MOVE THIS TO SEPARATE SIGNUP/CREATE USER PAGE
+  handleSignUp = async e => {
+    e.preventDefault();
+    try {
+      const { token } = await signUp(this.state.email, this.state.password);
+      this.props.handleTokenChange(token);
+      //fill in redirect to add a pet 
+    }
+    catch (e) {
+      this.setState({ error: e.response.body.error });
+      this.state.error === 'email and password required' && alert('Please provide both an email and password.');
+      this.state.error === 'email already exists' && alert(`This profile already exists. Please go to login instead or create a new user.`);
+      console.log(this.state.error);
+    }
+  }
+
+  //login submit
+  handleLogIn = async e => {
+    e.preventDefault();
+    try {
+      const { token } = await logIn(this.state.email, this.state.password);
+      this.props.handleTokenChange(token);
+      //fill in redirect to gallery 
+    }
+    catch (e) {
+      this.setState({ error: e.response.body.error });
+      this.state.error === 'email and password required' && alert('Please provide both an email and password.');
+      this.state.error === 'email or password incorrect' && alert(`Incorrect credentials. Please try again.`);
+      console.log(this.state.error);
+    }
+  }
+
+
+  //ADD LOGIN FORM WITH NOTE - new here? create your profile (link to sign up page)
   render() {
     return (
       <div>
+      
                 
       </div>
     );
