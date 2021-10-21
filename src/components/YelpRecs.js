6d4dcf3541c77aa9cfb233add101bebc114a7ccf
diyanamendoza <diyana.amp@pm.me>
2021-10-21 16:12:24 -0700

    YelpRec component code cleanup

--- src/components/YelpRecs.js
+++ src/components/YelpRecs.js
@@ -19,22 +19,13 @@ export default class YelpRecs extends Component {
     const park = findBusiness(businesses, 'parks');
     await this.setState({ park });
 
-    // const photography = businesses.find(business => business.category.find(category => category.alias === 'petphotography'));
-    // await this.setState({ photography });
-
     const groomer = findBusiness(businesses, 'groomer');
     await this.setState({ groomer });
-
-    // const adoption = findBusiness(businesses, 'petadoption');
-    // await this.setState({ groomer });
-
-
     
   }
   
   render() {
     const park = this.state.park;
-    // const photography = this.state.photography;
     const groomer = this.state.groomer;
     
     return (
@@ -46,7 +37,9 @@ export default class YelpRecs extends Component {
           <p>About {park.distance} mi away</p>
           <a href={park.url}>View on Yelp</a> 
         </div>
+
         <hr/>
+        
         <div className="yelp-rec">
           <h3>A spa-w day is in order</h3>
           <img src="../Icons/Pets/nail-file.png" alt="map icon"/>
